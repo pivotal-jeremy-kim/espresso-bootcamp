1bc01c2053f82cf4bff9eb75f89fc9134192c0a6
pivotal-jeremy-kim <jekim@pivotal.io>
2016-01-20 14:58:37 -0500

    Task #2

--- app/src/androidTest/java/com/example/weatherapp/ProjectSunshineEspressoTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ProjectSunshineEspressoTest.java
@@ -2,6 +2,7 @@ package com.example.weatherapp;
 
 import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -12,13 +13,23 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasSibling;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 
 /**
  * Created by pivotal on 2016-01-19.
@@ -32,7 +43,7 @@ public class ProjectSunshineEspressoTest {
 
     @Test
     public void task0() {
-        SystemClock.sleep(10000);
+        SystemClock.sleep(1000);
     }
 
     @Test
@@ -40,5 +51,21 @@ public class ProjectSunshineEspressoTest {
         openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
         onView(withText(R.string.action_settings)).perform(click());
         onView(withText(R.string.preference_title)).check(matches(withText("Forecast Preference")));
+    }
+
+    @Test
+    public void task2() {
+        String city;
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_zip_title)).perform(click());
+        onView(withId(R.id.edittext_container)).perform(click());
+        onView(withId(android.R.id.edit)).perform(clearText(), typeText("Chicago"));
+        onView(withText("OK")).perform(click());
+        Espresso.pressBack();
+        SystemClock.sleep(500);
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(allOf(withId(android.R.id.summary), hasSibling(withText("Set location")))).check(matches(withText("Chicago")));
     }
 }
