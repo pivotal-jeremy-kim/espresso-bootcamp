490871a60dfa81cb48821a2b0c5f160001b25692
pivotal-jeremy-kim <jekim@pivotal.io>
2016-01-20 10:32:02 -0500

    Task #1

--- app/src/androidTest/java/com/example/weatherapp/ProjectSunshineEspressoTest.java
+++ app/src/androidTest/java/com/example/weatherapp/ProjectSunshineEspressoTest.java
@@ -1,6 +1,7 @@
 package com.example.weatherapp;
 
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -12,9 +13,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static java.util.regex.Pattern.matches;
 
 /**
  * Created by pivotal on 2016-01-19.
@@ -22,12 +26,19 @@ import static java.util.regex.Pattern.matches;
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 
-public class Test0EspressoTest {
+public class ProjectSunshineEspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
 
     @Test
-    public void listGoesOverTheFold() {
+    public void task0() {
         SystemClock.sleep(10000);
+    }
+
+    @Test
+    public void task1() {
+        openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getTargetContext());
+        onView(withText(R.string.action_settings)).perform(click());
+        onView(withText(R.string.preference_title)).check(matches(withText("Forecast Preference")));
     }
 }
